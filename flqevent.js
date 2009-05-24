f989e2a44ca877f8f890dd29e31ae6efff92b7c8
cbiggins <cbiggins@gmail.com>
2009-05-24 04:41:44 -0700

    Removed semicolons, shortened some var names, added the methods stopEvent, StopProp & PrevDef.

--- flqevent.js
+++ flqevent.js
@@ -18,60 +18,92 @@
     /**
      * FLQ.event - Fliquid event handling library
      *
-     * Version: 1.0.1 BETA
-     * Last Modified: 31/12/2008
+     * Version: 1.1.0 BETA
+     * Last Modified: 24/05/2009
      */
 
-    if (typeof FLQ == 'undefined') var FLQ = new Object();
+    if (typeof FLQ == 'undefined') var FLQ = new Object()
 
-    FLQ.event = new Object();
+    FLQ.event = new Object()
 
     /**
-        * addListener() will add a listener to an element for a given event
-        */
-    FLQ.event.addListener = function(ele, event, functionRef, capture) {
+     * addListener() will add a listener to an element for a given event
+     */
+    FLQ.event.addListener = function(ele, event, fRef, cap) {
         if (typeof ele == 'string') {
-            ele = document.getElementById(ele);
+            ele = document.getElementById(ele)
         }
         if (typeof ele.addEventListener != 'undefined') {
-            ele.addEventListener(event, functionRef, capture);
+            ele.addEventListener(event, fRef, cap)
         } else if (typeof ele.attachEvent != 'undefined') {
-            ele.attachEvent('on' + event, functionRef);
+            ele.attachEvent('on' + event, fRef)
         }
-    }    
-    
+    }
+
+    FLQ.event.add = function(ele, event, fRef, cap) { FLQ.event.addListener(ele, event, fRef, cap) }
+
     /**
-        * getXY() will return an array of the X and Y coord's of the event
-        */
+     * getXY() will return an array of the X and Y coord's of the event
+     */
     FLQ.event.getXY = function(e) {
         if (typeof e == 'undefined') {
-            e = window.event;
+            e = window.event
         }
-        var xy = [e.screenX, e.screenY];
-        return xy;
+        var xy = [e.screenX, e.screenY]
+        return xy
     }
 
     /**
-        * removeListener() will remove a listener from an element for a given event
-        */
-    FLQ.event.removeListener = function(ele, event, functionRef, capture) {
+     * removeListener() will remove a listener from an element for a given event
+     */
+    FLQ.event.removeListener = function(ele, event, fRef, capture) {
         if (typeof ele == 'string') {
-            ele = document.getElementById(ele);
+            ele = document.getElementById(ele)
         }
         if (typeof ele.removeEventListener != 'undefined') {
-            ele.removeEventListener(event, functionRef, capture);
+            ele.removeEventListener(event, fRef, capture)
         } else if (typeof ele.detachEvent != 'undefinded') {
-            ele.detachEvent('on' + event, functionRef);
+            ele.detachEvent('on' + event, fRef)
         }
     }
 
     /**
-        * getTarget returns the target element of the event.
-        */
+     * getTarget returns the target element of the event.
+     */
     FLQ.event.getTarget = function(e) {
         if (typeof e.target != 'undefined') {
-            return e.target;
+            return e.target
         } else if (typeof e.srcElement != 'undefined') {
-            return e.srcElement;
+            return e.srcElement
         }
     }
+
+    /**
+     * stopProp will stop the event from propagating or bubbling.
+     */
+    FLQ.event.stopProp = function(e) {
+        if (typeof e.stopPropagation != 'undefined') {
+            e.stopPropagation()
+        } else if (typeof e.cancelBubble != 'undefined') {
+            e.cancelBubble = true
+        }
+    }
+	
+	/**
+	 * prevDef will prevent the default event action from occuring (preventDefault)
+	 */
+	FLQ.event.prevDef = function(e) {
+		if (typeof e.preventDefault != 'undefined') {
+			e.preventDefault()
+		} else {
+			e.returnValue = false
+		}
+	}
+	
+	/**
+	 * stopEvent is a wrapper for stopProp and prevDef methods
+	 */
+	FLQ.event.stopEvent(e) {
+		FLQ.event.stopProp(e)
+		FLQ.event.prevDef(e)
+	}
